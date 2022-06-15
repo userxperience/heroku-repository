9e2db182e14d6b228a751a7422ee22c2ca32ce35
userxperience <tijnvisser3@gmail.com>
2022-06-15 11:20:49 +0200

    work

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,7 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Article;
-use App\Models\post;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
